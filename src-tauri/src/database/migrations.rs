415395c3b7f235f569791ad8109aa01e3dec721c
Shane Snover <ssnover95@gmail.com>
2023-08-25 23:08:45 -0600

    Add migrations

---
+++ src-tauri/src/database/migrations.rs
@@ -0,0 +1,19 @@
+pub fn perform_migration(
+    db_conn: &mut rusqlite::Connection,
+    starting_version: i32,
+) -> rusqlite::Result<()> {
+    match starting_version {
+        1 => migrate_from_1_to_2(db_conn),
+        ver => {
+            log::error!("Request to migrate invalid database version {ver}");
+            Err(rusqlite::Error::InvalidQuery)
+        }
+    }
+}
+
+fn migrate_from_1_to_2(db_conn: &mut rusqlite::Connection) -> rusqlite::Result<()> {
+    log::debug!("Beginning migration 1 to 2");
+    let txn = db_conn.transaction()?;
+    let _row_changed = txn.execute("ALTER TABLE saves ADD COLUMN connected DEFAULT 1", ())?;
+    txn.commit()
+}
